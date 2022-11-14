9d4097d1724a5853a3bb62cba88f7c2481309ce2
Leo <47742726+leonid1123@users.noreply.github.com>
2022-11-14 10:36:01 +0300

    шифр

--- Program.cs
+++ Program.cs
@@ -5,14 +5,35 @@ Console.WriteLine("Пора шифровать по методу сдвига");
 string userInputText = "";
 string userInputNumber = "";
 int sdvig = 0;
-do {
+do
+{
     Console.WriteLine("Введите слово и нажмите ENTER");
     userInputText = Console.ReadLine();
 } while (userInputText.Length < 1);
-do {
+do
+{
     Console.WriteLine("Введите сдвиг в виде целого числа и нажмите ENTER");
     userInputNumber = Console.ReadLine();
 } while (!int.TryParse(userInputNumber, out sdvig));
-Console.WriteLine("Текст для шифрования:{0}",userInputText);
-Console.WriteLine("Сдвиг шифра:{0}",sdvig);
+Console.WriteLine("Текст для шифрования:{0}", userInputText);
+Console.WriteLine("Сдвиг шифра:{0}", sdvig);
 string abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+string abcShifr = "";
+for (int i = 0; i < abc.Length; i++)
+{
+    int x = i + sdvig;
+    if (x > 32)
+    {
+        x -= 33;
+    }
+    abcShifr += abc[x];
+}
+Console.WriteLine("Шифрованный алфавит:{0}", abcShifr);
+string shifr = "";
+for(int i = 0; i < userInputText.Length; i++)
+{
+    shifr += abcShifr[abc.IndexOf(userInputText[i])];
+}
+Console.WriteLine("Зашифрованное слово:{0}",shifr);
+//решить проблемы:большие буквы, пробел, знаки припенания, латиница
+//сделать запись в БД
