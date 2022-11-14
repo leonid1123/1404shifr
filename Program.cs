c32af5fabc251aa48c13394e4ca6e72483a4c195
Leo <47742726+leonid1123@users.noreply.github.com>
2022-11-14 09:17:18 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//1. спросить у пользователя слово - 1 шт, на кирилице без пробелов и знаков припенаний
+//2. зашифровать
+//3. напечатать исходное слово и зашифрованное
+Console.WriteLine("Пора шифровать по методу сдвига");
+string userInputText = "";
+string userInputNumber = "";
+int sdvig = 0;
+do {
+    Console.WriteLine("Введите слово и нажмите ENTER");
+    userInputText = Console.ReadLine();
+} while (userInputText.Length < 1);
+do {
+    Console.WriteLine("Введите сдвиг в виде целого числа и нажмите ENTER");
+    userInputNumber = Console.ReadLine();
+} while (!int.TryParse(userInputNumber, out sdvig));
+Console.WriteLine("Текст для шифрования:{0}",userInputText);
+Console.WriteLine("Сдвиг шифра:{0}",sdvig);
+string abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
